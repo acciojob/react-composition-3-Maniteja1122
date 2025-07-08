84320d5c58d70e45dae4efb4c705bcea333beed5
Maniteja1122 <kuntamaniteja0607@gmail.com>
2025-07-08 19:02:32 +0530

    updated tooltip component

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -4,21 +4,19 @@ const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
 
   return (
-    <div
-      className="tooltip"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
-      {children}
-      <span
-        className="tooltiptext"
-        style={{
-          visibility: show ? "visible" : "hidden",
-          opacity: show ? 1 : 0,
-        }}
-      >
-        {text}
-      </span>
+    <div onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+      <div className="tooltip">
+        {children}
+        <span
+          className="tooltiptext"
+          style={{
+            visibility: show ? "visible" : "hidden",
+            opacity: show ? 1 : 0,
+          }}
+        >
+          {text}
+        </span>
+      </div>
     </div>
   );
 };
